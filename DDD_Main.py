e567d2bfa153fcc4fedfe13558ac144ed7050c66
Ibrahim Sel <ibrahim.sel58@hotmail.com>
2022-12-27 16:55:39 +0300

    initial commit

---
+++ DDD_Main.py
@@ -0,0 +1,132 @@
+import cv2
+import time
+import numpy as np
+import mediapipe as mp
+import tensorflow as tf
+from playsound import playsound
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_face_mesh = mp.solutions.face_mesh
+
+left_eye = mp_face_mesh.FACEMESH_LEFT_EYE
+right_eye = mp_face_mesh.FACEMESH_RIGHT_EYE
+denormalize_coordinates = mp_drawing._normalized_to_pixel_coordinates
+
+#                    sol üst   sağ alt
+roi_eye_left = [276, 285, 343, 346]
+
+forehead = [10]
+
+start_time = 0
+drowsy_time = 0
+# Specifications for drawing the lines
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
+model = tf.keras.models.load_model('models/CNN-163216-80k.h5')
+alarm_sound = 'alarm_sound.mp3'
+
+cv2.namedWindow('Driver Drowsiness Detection', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Driver Drowsiness Detection', 640, 480)
+
+drowsy_time = 0
+
+cap = cv2.VideoCapture(0)
+with mp_face_mesh.FaceMesh(
+        static_image_mode=True,
+        max_num_faces=1,
+        refine_landmarks=True,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as face_mesh:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
+        cv2.putText(image, "Press 'q' to quit", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        results = face_mesh.process(image)
+
+        if results.multi_face_landmarks:
+            for face in results.multi_face_landmarks:
+                landmarks = face.landmark
+
+                f = {}
+                for index in forehead:
+                    x = int(landmarks[index].x * image.shape[1])
+                    y = int(landmarks[index].y * image.shape[0])
+                    f[index] = (x, y)
+
+                e = {}
+                for index in roi_eye_left:
+                    x = int(landmarks[index].x * image.shape[1])
+                    y = int(landmarks[index].y * image.shape[0])
+                    e[index] = (x, y)
+
+                    """
+                    -------------------------------------------
+                    |                                         | 
+                    |    (x1, y1)                             |
+                    |      ------------------------           |
+                    |      |                      |           |
+                    |      |                      |           | 
+                    |      |         ROI          |           |  
+                    |      |                      |           |   
+                    |      |                      |           |   
+                    |      |                      |           |       
+                    |      ------------------------           |   
+                    |                           (x2, y2)      |    
+                    |                                         |             
+                    |                                         |             
+                    |                                         |             
+                    -------------------------------------------
+
+                    ROI = image[y1:y2, x1:x2]
+                    """
+
+                # 285 sol üst 346 sağ alt
+                cropped_left_eye = image[e[285][1]
+                    :e[346][1], e[285][0]:e[346][0]]
+
+                if cropped_left_eye.size == 0:
+                    continue
+                eye_roi = cv2.resize(cropped_left_eye, (256, 256))
+                eye_roi = eye_roi / 255.0
+                eye_roi = np.expand_dims(eye_roi, axis=0)
+                prediction = model.predict(eye_roi)
+
+                if prediction > 0.5:
+                    cv2.putText(image, f"Eyes Open {prediction[0][0]:.2f}", f[10],
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                    start_time = time.perf_counter()
+                    drowsy_time = 0
+                else:
+                    cv2.putText(image, f"Eyes Closed {prediction[0][0]:.2f}", f[10],
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    end_time = time.perf_counter()
+                    drowsy_time += end_time - start_time
+                    start_time = end_time
+
+                if drowsy_time > 2:
+                    cv2.putText(image, "DROWSINESS ALERT!", (10, 200),
+                                cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 2)
+                    # playsound(alarm_sound)
+                    
+                elif drowsy_time > 0 and drowsy_time < 2:
+                    cv2.putText(image, "BLINK", (10, 200),
+                                cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 2)
+
+                # print drowsy_time
+                cv2.putText(image, f"Drowsy Time: {drowsy_time:.2f}", (10, 400),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+                for index in roi_eye_left:
+                    cv2.circle(image, e[index], 2, (255, 255, 255), -1)
+
+        cv2.imshow('Driver Drowsiness Detection', image)
+        if ord('q') == cv2.waitKey(1):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ DDD_WebApp.py
@@ -0,0 +1,105 @@
+import cv2
+import time
+import numpy as np
+import mediapipe as mp
+import tensorflow as tf
+import streamlit as st
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_face_mesh = mp.solutions.face_mesh
+
+denormalize_coordinates = mp_drawing._normalized_to_pixel_coordinates
+
+# 285: top left point of left eye / 346: bottom right of left eye
+roi_eye_left = [276, 285, 343, 346]
+
+forehead = [10]
+
+start_time = 0
+drowsy_time = 0
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
+model = tf.keras.models.load_model('models/CNN-163216-80k.h5')
+drowsy_time = 0
+
+st.title("Drowsiness Detection")
+st.write("This is a simple drowsiness detection app using mediapipe and tensorflow.")
+
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+
+cap = cv2.VideoCapture(0)
+with mp_face_mesh.FaceMesh(
+        static_image_mode=True,
+        max_num_faces=1,
+        refine_landmarks=True,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as face_mesh:
+    while run:
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
+        cv2.putText(image, "Press 'q' to quit", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        results = face_mesh.process(image)
+
+        if results.multi_face_landmarks:
+            for face in results.multi_face_landmarks:
+                landmarks = face.landmark
+
+                f = {}
+                for index in forehead:
+                    x = int(landmarks[index].x * image.shape[1])
+                    y = int(landmarks[index].y * image.shape[0])
+                    f[index] = (x, y)
+
+                e = {}
+                for index in roi_eye_left:
+                    x = int(landmarks[index].x * image.shape[1])
+                    y = int(landmarks[index].y * image.shape[0])
+                    e[index] = (x, y)
+
+                cropped_left_eye = image[e[285][1]
+                    :e[346][1], e[285][0]:e[346][0]]
+
+                if cropped_left_eye.size == 0:
+                    continue
+                eye_roi = cv2.resize(cropped_left_eye, (256, 256))
+                eye_roi = eye_roi / 255.0
+                eye_roi = np.expand_dims(eye_roi, axis=0)
+                prediction = model.predict(eye_roi)
+                if prediction < 0.5:
+                    cv2.putText(image, f"Eyes Open {prediction[0][0]:.2f}", f[10],
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                    start_time = time.perf_counter()
+                    drowsy_time = 0
+                else:
+                    cv2.putText(image, f"Eyes Closed {prediction[0][0]:.2f}", f[10],
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    end_time = time.perf_counter()
+                    drowsy_time += end_time - start_time
+                    start_time = end_time
+
+                if drowsy_time > 2:
+                    cv2.putText(image, "DROWSINESS ALERT!", (10, 200),
+                                cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 2)
+                elif drowsy_time > 0 and drowsy_time < 2:
+                    cv2.putText(image, "BLINK", (10, 200),
+                                cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 2)
+
+                cv2.putText(image, f"Drowsy Time: {drowsy_time:.2f}", (10, 400),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+                            
+                for index in roi_eye_left:
+                    cv2.circle(image, e[index], 2, (255, 255, 255), -1)
+
+        FRAME_WINDOW.image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        if ord('q') == cv2.waitKey(1):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
