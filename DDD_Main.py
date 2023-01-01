6ac3ad84b06a5ccc7723d525f07898b66bdc5a94
ibrahim sel <ibrahim.sel58@hotmail.com>
2023-01-01 22:33:27 +0300

    removed some unnecessary variables in main file

--- DDD_Main.py
+++ DDD_Main.py
@@ -5,11 +5,7 @@ import mediapipe as mp
 import tensorflow as tf
 from playsound import playsound
 
-mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
-
-left_eye = mp_face_mesh.FACEMESH_LEFT_EYE
-right_eye = mp_face_mesh.FACEMESH_RIGHT_EYE
 
 #                    top left   bottom right
 roi_eye_left = [276, 285, 343, 346]
